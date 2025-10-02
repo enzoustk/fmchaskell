f6f719f280a1cc6d7e1fd953fd4f1bf14c66c60f
Enzo <enzo@strikerinvest.com>
2025-10-02 11:53:24 -0300

    Tentativa de Resolução

---
+++ Solutions/FMCFun.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE GADTs #-}
+
+module ExList where
+
+import Prelude hiding
+    ( (.) , ($)
+    , flip , curry , uncurry
+    , iterate
+    )
+
+-- use your mind to infer the types, don't cheat!
+
+-- curry takes a "traditional" binary function
+-- and returns its currified version
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f x y = f (x, y)
+
+-- uncurry takes a currified function
+-- and returns its "traditional" binary version
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (x, y) = f x y
+
+-- flip takes a (currified) binary function
+-- and returns one that behaves the same but takes its arguments in the opposite order
+
+-- (.) takes two composable functions and returns their composition
+(.) :: (b -> c) -> (a -> b) -> a -> c
+(f . g) x = f (g x)
+
+infixr 9 .
+
+
+-- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
+(.>) = flip (.)
+
+flip :: (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+-- ($) takes a function and a suitable argument and applies the function to the argument
+-- think: why would we ever want that?
+
+-- iterate: figure it out by its type
+iterate :: (a -> a) -> a -> [a]
+iterate f x = x : iterate f (f x)
+
+-- orbit
+orbit :: a -> (a -> a) -> [a]
+orbit = flip iterate
